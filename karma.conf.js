56737633a2e3ea61b6be486350afd7af6cc8ecc7
pkn <pavannelakuditi@gmail.com>
2015-09-21 18:09:01 -0400

    basic karma conf added

---
+++ karma.conf.js
@@ -0,0 +1,12 @@
+module.exports = function(config) {
+    config.set({
+        basePath: './',
+        frameworks: ['jasmine'],
+        browsers : ['Chrome'],
+
+        plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+        ]
+    });
+};
